b62f0657d7f33e9c2f0bf94c0911696a0960d5e7
Tori-coder <114353884+Tori-coder@users.noreply.github.com>
2025-04-07 20:51:48 +0100

    install gunicorn and create procfile for heroku

--- django_project_4/settings.py
+++ django_project_4/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j)2j@y009s3y06jxs%ar%svkml3j_tqos1rbt+a=pp8$9sbm59'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.herokuapp.com',
+]
 
 
 # Application definition
