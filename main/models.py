fac896e1e41d1137cbdd89ce7f71c444f3e8d8a5
Tori-coder <114353884+Tori-coder@users.noreply.github.com>
2025-04-08 14:52:53 +0100

    delete unneccesary id field in models.CourseTitle

--- main/models.py
+++ main/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class CourseTitle(models.Model):
-    id = models.PositiveIntegerField(primary_key=True)
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True)
     author = models.ForeignKey(User, on_delete=models.PROTECT)
