9b837def3fecfa3279da2a77defb64705e0fadca
Tori-coder <114353884+Tori-coder@users.noreply.github.com>
2025-05-02 18:45:14 +0100

    amend signals.py

--- main/signals.py
+++ main/signals.py
@@ -1,5 +1,5 @@
 from allauth.account.signals import user_logged_in
-from allauth.socialaccount.signals import user_logged_up
+from allauth.socialaccount.signals import user_signed_up
 from django.dispatch import receiver
 from .models import StudentProfile
 
@@ -8,7 +8,7 @@ def create_student_profile(sender, request, user, **kwargs):
     if not hasattr(user, 'studentprofile'):
         StudentProfile.objects.create(user=user, level='A1')
 
-@receiver(user_logged_up)
+@receiver(user_signed_up)
 def create_student_profile(sender, request, user, **kwargs):
     if not hasattr(user, 'studentprofile'):
         StudentProfile.objects.create(user=user, level='A1')
