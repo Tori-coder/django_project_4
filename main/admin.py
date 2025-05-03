d1a06dd1f24c937b0905f6ec65ef61a27d7785ae
Tori-coder <114353884+Tori-coder@users.noreply.github.com>
2025-05-03 19:59:01 +0100

    fix declaration order in admin.py

--- main/admin.py
+++ main/admin.py
@@ -4,9 +4,10 @@ from .models import CourseTitle, CourseType, Enrolment #, CourseTitleTypeNexus
 admin.site.register(CourseTitle)
 admin.site.register(CourseType)
 # admin.site.register(CourseTitleTypeNexus)
-admin.site.register(Enrolment, EnrolmentAdmin)
 
 class EnrolmentAdmin(admin.ModelAdmin):
     enrolment_display=('student', 'course_title', 'enrolment_date')
     enrolment_filter=('student', 'course_title')
     search_by = ('student__username', 'course_title__title')
+
+admin.site.register(Enrolment, EnrolmentAdmin)
