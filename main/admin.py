e54f44794f3a32e81542fe9da2fc96ec54b36a61
Tori-coder <114353884+Tori-coder@users.noreply.github.com>
2025-05-03 19:52:12 +0100

    update admin.py to allow viewing of enrolments

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import CourseTitle, CourseType #, CourseTitleTypeNexus, Enrolment
+from .models import CourseTitle, CourseType, Enrolment #, CourseTitleTypeNexus
 
 admin.site.register(CourseTitle)
 admin.site.register(CourseType)
 # admin.site.register(CourseTitleTypeNexus)
-# admin.site.register(Enrolment)
+admin.site.register(Enrolment, EnrolmentAdmin)
+
+class EnrolmentAdmin(admin.ModelAdmin):
+    enrolment_display=('student', 'course_title', 'enrolment_date')
+    enrolment_filter=('student', 'course_title')
+    search_by = ('student__username', 'course_title__title')
