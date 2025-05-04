990012b76c2daf7726675cb934980458bd165b3e
Tori-coder <114353884+Tori-coder@users.noreply.github.com>
2025-05-04 13:59:26 +0100

    fix login conflict error with allauth

--- main/urls.py
+++ main/urls.py
@@ -9,8 +9,6 @@ urlpatterns = [
     path("course_detail/<int:course_id>", views.course_detail, name="course_detail"),
     path("course_content/<int:course_id>", views.course_content, name="course_content"),
     path("register/", views.register, name="register"),
-    path("login/", auth_views.LoginView.as_view(template_name='main/login.html'), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(next_page='home'), name="logout"),
     path('profile/', views.student_profile, name='student_profile'),
     path("profile/edit/", views.edit_profile, name="edit_profile"),
     path("profile/delete/", views.delete_profile, name="delete_profile"),
